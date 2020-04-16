9d44bbc1fc7a6436ef17dfc1a21b422093f64c08
Jacob Haller-Roby <jroby@staywell.com>
2020-04-15 17:24:22 -0700

    Cleanup on package file

--- package.js
+++ package.js
@@ -2,13 +2,11 @@ const path = require('path');
 
 const resourceHackerPath = path.join(__dirname, './node_modules/node-resourcehacker/ResourceHacker.exe');
 const pkgCachePath = "./pkg-cache";
-process.env["SOURCE_RESOURCE_HACKER"] = resourceHackerPath;
 process.env["PKG_CACHE_PATH"] = pkgCachePath;
 
 const execSync = require("child_process").execSync;
 
 const fs = require("fs-extra");
-const pkg = require("pkg");
 const pkgfetch = require("pkg-fetch");
 
 const customIconPath = "iconset.ico";
@@ -17,35 +15,36 @@ const originalPkgPrecompiledBinaries =`${pkgCachePath}/v2.6/fetched-v${specificN
 const customizedPkgPrecompiledBinaries =`${pkgCachePath}/v2.6/fetched-v${specificNodeVersion}-win-x64`;
 
 async function downloadOriginalPkgPrecompiledBinaries() {
-    if (!fs.existsSync(originalPkgPrecompiledBinaries)) {
+    if (!fs.existsSync(customizedPkgPrecompiledBinaries)) {
         await pkgfetch.need({nodeRange:`node${specificNodeVersion}`,platform:"win",arch:"x64"});
     }
 }
+
+async function renamePCachePackages() {
+    await fs.rename(
+        customizedPkgPrecompiledBinaries,
+        originalPkgPrecompiledBinaries
+    )
+}
+
 async function customizePkgPrecompiledBinariesIcon() {
     execSync(`${resourceHackerPath} -open ./pkg-cache/v2.6/fetched-v${specificNodeVersion}-win-x64.original -save ./pkg-cache/v2.6/fetched-v${specificNodeVersion}-win-x64 -resource ./${customIconPath} -action addoverwrite -mask ICONGROUP,1,`);
 }
-async function customizePkgPrecompiledBinaries() {
-    if(!fs.existsSync(originalPkgPrecompiledBinaries)){
-        await fs.rename(
-            customizedPkgPrecompiledBinaries,
-            originalPkgPrecompiledBinaries
-        );
-    }
-    await customizePkgPrecompiledBinariesIcon();
+
+async function buildApp() {
+    execSync(
+        "pkg ./dist/App.min.js --target win --output \"Zoom Lights.exe\""
+    );
 }
+
 
 async function package() {
-    console.log("Download pkg precompiled libraries");
     await downloadOriginalPkgPrecompiledBinaries()
-        .then(customizePkgPrecompiledBinaries)
-        .then(() => {
-            execSync(
-                "pkg ./dist/App.min.js --target win --output \"Zoom Lights.exe\""
-            );
-        })
-        .then(() => console.log('COMPLETE!'))
-        .catch(error => console.error(error));
-
+        .then(renamePCachePackages)
+        .then(customizePkgPrecompiledBinariesIcon)
+        .then(buildApp);
 }
 
-package();
+package()
+    .then(() => console.log('COMPLETE!'))
+    .catch(error => console.error(error));
